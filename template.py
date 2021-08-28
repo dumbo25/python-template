baf95f7389a4f9954280704edbecf4e144d14155
dumbo25 <dumbo25@users.noreply.github.com>
2021-08-28 11:27:53 -0500

    Create template.py
    initial commit of template

---
+++ template.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python3
+"""
+use docstrings
+
+Modules or Tools that must be installed for script to work:
+    ???
+
+References:
+    ???
+
+My Guidelines for pythonic code:
+    https://sites.google.com/site/cartwrightraspberrypiprojects/home/footer/pythonic
+"""
+
+############################# <- 80 Characters -> ##############################
+#
+# This style of commenting is called a garden bed and it is frowned upon by
+# those who know python. But, I like it. I use a garden bed at the top to track
+# my to do list as I work on the code, and then mark the completed To Dos. 
+#
+# Within the code, To Dos include a comment with ???, which makes it easy for 
+# me to find
+#
+# Once the To Do list is more-or-less complete, I move the completed items into
+# comments, docstrings or help within the code. Next, I remove the completed
+# items.
+#
+# To Do List:
+#   a)
+#     a.1
+#
+# Do later or not at all (I am tired of working on this):
+#
+# Won't Do:
+#
+# Completed:
+#
+############################# <- 80 Characters -> ##############################
+
+# Built-in Python3 Modules
+
+# Modules that must be installed (pip3)
+
+# My Modules (from github)
+
+# Global Veriables (FirstUpper or CapWord)
+
+# Global Constatnts (ALL CAPS)
+
+# Classes
+
+# function definitions
+def main():
+
+# In every script, ALWAYS use an if main
+if __name__ == "__main__":
+    main()
